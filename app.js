669ffcaf6a2e5052b69cb3087c743df9537b5170
Ed <lookatemail@gmail.com>
2020-12-11 23:06:35 +0000

    Load Environment Variables from a .env File

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require('path');
 const bodyParser = require('body-parser'); // importing body parser middleware to parse form content from HTML
 const nodemailer = require('nodemailer'); //importing node mailer
 
+const dotenv = require('dotenv'); // Load Environment Variables from a .env File
+dotenv.config();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public'))); // serving our contact form on '/' route
 // route which captures form details and sends it to your personal mail
-
-const nodeAccount = 'automated.nodemailer@gmail.com';
-const mailList = 'lookatemail@gmail.com, ed.ancerys@ndg-technology.com';
 
 app.post('/sendEmail', (req, res, next) => {
   console.log(req.body);
@@ -19,10 +19,10 @@ here we are using gmail as our service
 In Auth object , we specify our email and password
 */
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: process.env.EMAIL_SERVICE,
     auth: {
-      user: nodeAccount, //replace with your email
-      pass: 'password@nodemailer', //replace with your password
+      user: process.env.EMAIL_NAME, //replace with your email
+      pass: process.env.EMAIL_PASSWORD, //replace with your password
     },
   });
   /*
@@ -32,8 +32,8 @@ Subject is Contact name and
 html is our form details which we parsed using bodyParser.
 */
   const mailOptions = {
-    from: nodeAccount, //replace with your email
-    to: mailList, //replace with your email
+    from: process.env.EMAIL_NAME, //replace with your email
+    to: process.env.MAILING_LIST, //replace with your email
     subject: `Contact name: ${req.body.name}`,
     html: `<h1>Contact details</h1>
 <h2> name:${req.body.name} </h2><br>
