57467f0e09318bba642bbd336b6c7a20af045305
Ed <lookatemail@gmail.com>
2020-12-12 13:13:45 +0000

    Update package.json - change main file link

--- public/JS/index.js
+++ public/JS/index.js
@@ -2,7 +2,7 @@ console.log('index.js loaded...');
 
 document.getElementById('contact').addEventListener('click', (event) => {
   event.preventDefault();
-  console.log('click');
+  console.log('click and click');
 
   // document.getElementById('testFormMessage').value = '';
   // console.log(document.getElementById('testForm').elements['message'].value);
