2382c70867ffa94640d30f9742d876d762c8dd16
Ed <lookatemail@gmail.com>
2020-12-12 12:23:07 +0000

    Able to clear form field values on submit

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,5 +1,7 @@
 console.log('index.js loaded...');
 
-document.getElementById('contact').addEventListener('click', function (event) {
+document.getElementById('testForm').addEventListener('click', function (event) {
   event.preventDefault();
+  document.getElementById('testFormMessage').value = '';
+  console.log(document.getElementById('testForm').elements['message'].value);
 });
